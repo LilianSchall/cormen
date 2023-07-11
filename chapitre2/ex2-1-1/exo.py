ab41cdc4b4cf99fbe8c52fe2a93d4063f917be0a
Lilian Schall <lilian.schall@epita.fr>
2023-07-11 12:56:48 +0200

    feat: finished exercice 2-1-1

---
+++ chapitre2/ex2-1-1/exo.py
@@ -0,0 +1,20 @@
+def tri_insertion(A, cmp=(lambda x,y: x < y), print_mode=False):
+    for j in range(1, len(A)):
+        key = A[j]
+        # on insere A[j] dans la sequence triee A[1..j-1]
+        i = j - 1
+        while i >= 0 and cmp(key, A[i]):
+            A[i + 1] = A[i]
+            i = i - 1
+        A[i + 1] = key
+        if print_mode:
+            print(A)
+
+A = [31, 41, 59, 26, 41, 58]
+print("---initial values---")
+print(A)
+print("--------------------")
+tri_insertion(A, print_mode=True)
+print("-----End values-----")
+print(A)
+print("--------------------")
